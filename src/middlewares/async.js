8804703733f194555fe592d75579c0c33b8159fb
Clint Ast <clintast@gmail.com>
2017-02-14 05:18:47 -0600

    added code to the async middleware to take returned data from promise and dispatch back through middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,17 @@
 export default function({despatch}) {
   return next => action => {
-    console.log(action);
-
-    next(action);
+    // if action doesn't have payload or, the payload
+    // doesn't have a .then property we don't care about
+    // it send it on.
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
+    // make sure the action's promise resolves
+    action.payload
+      .then(function(response ) {
+        //create a new action with the old type, but
+        // replace the promise with the response data
+        const newAction = {...action, payload: response}
+      });
   }
 }
